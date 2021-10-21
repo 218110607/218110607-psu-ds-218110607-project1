eb079de9174afbf1ea40a148bd0a3bfa27943cbc
macbook <macbook@192.168.100.23>
2021-10-21 19:15:36 +0300

    test the application using the main

--- psu-ds-218110607-project1.git/src/Application.java
+++ psu-ds-218110607-project1.git/src/Application.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,7 +17,27 @@ public class Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        System.out.println("--------------------");
+         System.out.println("number 1 :");
+        String numberstr1=input.nextLine();
+         System.out.println("number 2");
+        String numberstr2=input.nextLine();
+        BigInt number1= new BigInt(numberstr1);
+        BigInt number2= new BigInt(numberstr2);
+       
+        BigInt number3= number1.add(number2);
+       
+        BigInt number4= number1.subtract(number2);
+        
+        int isbigger= number1.compare(number2);
+
+        System.out.println("number1 : "+number1);
+        System.out.println("number1 : "+number2);
+        System.out.println("add result: "+number3);
+        System.out.println("Subtract result : "+number4);
+        System.out.println("Compare : "+isbigger);
+       
     }
     
 }
